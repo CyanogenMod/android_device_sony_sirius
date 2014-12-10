eedb342ed6675b092d3d165bfaaf383555471894
Shane Francis <bigbeeshane@gmail.com>
2014-12-10 07:31:37 +0000

    audio : move to building audio.primary
    * Update mixer_paths.xml to start TFA amp
    
    Change-Id: I3732d1f6ec0b2474a59095b2b5f848c7cb2535e9

--- sirius.mk
+++ sirius.mk
@@ -48,6 +48,7 @@ PRODUCT_COPY_FILES += \
    $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
+
 # Thermal manager
 PRODUCT_COPY_FILES += \
    $(LOCAL_PATH)/thermanager.xml:system/etc/thermanager.xml
